72de914e7674d8977e1a31ca88ef5347b29f1274
Support <leshem.guy+bb@gmail.com>
2022-07-07 15:47:27 +0300

    roomoofoo

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,12 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
 import java.time.Duration
 
 buildscript {
-    dependencies {
-        classpath(kotlin("gradle-plugin", embeddedKotlinVersion))
-    }
+    plugins {
+    alias libs.plugins.android.application apply false
+    alias libs.plugins.android.library apply false
+    alias libs.plugins.kotlin.android apply false
+    alias libs.plugins.kotlin.kapt apply false
+}
 }
 
 plugins {
